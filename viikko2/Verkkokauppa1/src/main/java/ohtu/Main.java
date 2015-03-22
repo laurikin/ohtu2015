6aa816b97cb9a7a052d07d37024492107449bb8f
Lauri Kinnunen <lkkinnunen@gmail.com>
2015-03-22 15:03:04 +0200

    t10

--- viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -5,15 +5,15 @@ import ohtu.verkkokauppa.Kirjanpito;
 import ohtu.verkkokauppa.Pankki;
 import ohtu.verkkokauppa.Varasto;
 import ohtu.verkkokauppa.Viitegeneraattori;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        Kirjanpito kirjanpito = new Kirjanpito();
-        Varasto varasto = new Varasto(kirjanpito);
-        Pankki pankki = new Pankki(kirjanpito);
-        Viitegeneraattori viitegeneraattori = new Viitegeneraattori();
-        Kauppa kauppa = new Kauppa(varasto, pankki, viitegeneraattori);
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
+
+        Kauppa kauppa = ctx.getBean(Kauppa.class);
 
         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
         kauppa.aloitaAsiointi();
@@ -32,6 +32,7 @@ public class Main {
         kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
 
         // kirjanpito
+        Kirjanpito kirjanpito = (Kirjanpito) ctx.getBean("kirjanpito");
         for (String tapahtuma : kirjanpito.getTapahtumat()) {
             System.out.println(tapahtuma);
         }
