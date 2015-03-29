a314f64b28e3f257663521e030e8fa081a28e359
Lauri Kinnunen <lkkinnunen@gmail.com>
2015-03-29 14:14:43 +0300

    Uuden käyttäjän rekisteröitymisen testit

--- viikko3/easyb/src/main/java/ohtu/services/AuthenticationService.java
+++ viikko3/easyb/src/main/java/ohtu/services/AuthenticationService.java
@@ -3,6 +3,7 @@ package ohtu.services;
 import ohtu.domain.User;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 import ohtu.data_access.UserDao;
 
 public class AuthenticationService {
@@ -39,8 +40,14 @@ public class AuthenticationService {
     }
 
     private boolean invalid(String username, String password) {
-        // validity check of username and password
+        return invalidPassword(password) || invalidUsername(username);
+    }
 
-        return false;
+    private boolean invalidPassword(String password) {
+        return password.length() < 8 || !password.matches(".*\\d+.*");
+    }
+
+    private boolean invalidUsername(String username) {
+        return username.length() < 3;
     }
 }
