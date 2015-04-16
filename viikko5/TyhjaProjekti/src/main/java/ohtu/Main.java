eb9b213bc9bb176f051a80c02baa051ceedbacd7
Lauri Kinnunen <lkkinnunen@gmail.com>
2015-04-16 14:46:57 +0300

    tehtavat 2 ja 3

--- viikko5/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ viikko5/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -1,7 +1,47 @@
 package ohtu;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.io.IOUtils;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String studentNr = "014087527";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        String url = "http://ohtustats-2015.herokuapp.com/students/"+studentNr+"/submissions";
+
+        HttpClient client = new HttpClient();
+        GetMethod method = new GetMethod(url);
+        client.executeMethod(method);
+
+        InputStream stream =  method.getResponseBodyAsStream();
+
+        String bodyText = IOUtils.toString(stream);
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+
+        System.out.println("opiskelijanumero " + studentNr);
+
+        System.out.println("");
+
+        int hours = 0;
+        int tehtavat = 0;
+        for (int i = 0; i < subs.length; i++) {
+            System.out.print("viikko " + (i + 1) + ": ");
+            System.out.print(subs[i]);
+            System.out.print("\n");
+            hours += subs[i].getHours();
+            tehtavat += subs[i].tehdytTehtavat();
+        }
+        System.out.println("");
+        System.out.println("yhteensä: " + tehtavat + " tehtavaa " + hours + " tuntia");
+
     }
 }
--- viikko5/TyhjaProjekti/src/main/java/ohtu/Submission.java
+++ viikko5/TyhjaProjekti/src/main/java/ohtu/Submission.java
@@ -1,7 +1,24 @@
 package ohtu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Submission {
     private String student_number;
+    private int hours;
+    private boolean[] a;
+    private boolean a1;
+    private boolean a2;
+    private boolean a3;
+    private boolean a4;
+    private boolean a5;
+    private boolean a6;
+    private boolean a7;
+    private boolean a8;
+    private boolean a9;
+    private boolean a10;
+    private boolean a11;
+    private boolean a12;
 
     public String getStudent_number() {
         return student_number;
@@ -11,9 +28,66 @@ public class Submission {
         this.student_number = student_number;
     }
 
+    public int getHours() {
+        return hours;
+    }
+
+    public int tehdytTehtavat() {
+        return tehtavat().size();
+    }
+
+    public String tehtavaString() {
+        StringBuilder s = new StringBuilder();
+        for (int t : tehtavat()) {
+            s.append(t).append(" ");
+        }
+        return s.toString();
+    }
+
     @Override
     public String toString() {
-        return student_number;
+        return "tehtyjä tehtäviä yhteensä: " + tehdytTehtavat() + ", aikaa kului: " + hours + ", tehdyt tehtavat: " + tehtavaString();
+    }
+
+    private List<Integer> tehtavat() {
+        List<Integer> tehtavat = new ArrayList();
+        if (a1) {
+            tehtavat.add(1);
+        }
+        if (a2) {
+            tehtavat.add(2);
+        }
+        if (a3) {
+            tehtavat.add(3);
+        }
+        if (a4) {
+            tehtavat.add(4);
+        }
+        if (a5) {
+            tehtavat.add(5);
+        }
+        if (a6) {
+            tehtavat.add(6);
+        }
+        if (a7) {
+            tehtavat.add(7);
+        }
+        if (a8) {
+            tehtavat.add(8);
+        }
+        if (a9) {
+            tehtavat.add(9);
+        }
+        if (a10) {
+            tehtavat.add(10);
+        }
+        if (a11) {
+            tehtavat.add(11);
+        }
+        if (a12) {
+            tehtavat.add(12);
+        }
+        return tehtavat;
     }
     
 }
