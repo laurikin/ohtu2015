ecba84a3aa3684769b70314b2e6a296aba17d732
Lauri Kinnunen <lkkinnunen@gmail.com>
2015-04-26 22:58:03 +0300

    tehtava5

--- viikko6/QueryLanguage1/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage1/src/main/java/statistics/Main.java
@@ -6,9 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
           
-        Matcher m = new And( new HasAtLeast(10, "goals"),
-                             new HasAtLeast(10, "assists"),
-                             new PlaysIn("PHI")
+        Matcher m = new And( new Or(
+                                new HasFewerThan(4, "goals"),
+                                new HasAtLeast(30, "assists")
+                            ),
+                            new And(
+                                new Not(new PlaysIn("PHI")),
+                                new Not(new PlaysIn("PIT"))
+                            )
         );
         
         for (Player player : stats.matches(m)) {
--- viikko6/QueryLanguage1/src/main/java/statistics/matcher/HasFewerThan.java
+++ viikko6/QueryLanguage1/src/main/java/statistics/matcher/HasFewerThan.java
@@ -1,15 +1,23 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package statistics.matcher;
 
 import java.lang.reflect.Method;
 import statistics.Player;
 
-public class HasAtLeast implements Matcher {
-    
+/**
+ *
+ * @author laurikin
+ */
+public class HasFewerThan implements Matcher {
+
     private int value;
     private String fieldName;
 
-    public HasAtLeast(int value, String category) {
+    public HasFewerThan(int value, String category) {
         this.value = value;
         fieldName = "get"+Character.toUpperCase(category.charAt(0))+category.substring(1, category.length());
     }
@@ -19,12 +27,12 @@ public class HasAtLeast implements Matcher {
         try {                                    
             Method method = p.getClass().getMethod(fieldName);
             int playersValue = (Integer)method.invoke(p);
-            return playersValue>=value;
+            return playersValue < value;
             
         } catch (Exception ex) {
             System.out.println(ex);
             throw new IllegalStateException("Player does not have field "+fieldName.substring(3, fieldName.length()).toLowerCase());
         }       
-    }    
+    }
     
 }
