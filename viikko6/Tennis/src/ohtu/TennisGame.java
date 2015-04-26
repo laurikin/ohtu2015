0bc60fc9e0b906ef9012b1f830a090f6a51bebb8
Lauri Kinnunen <lkkinnunen@gmail.com>
2015-04-26 22:17:31 +0300

    tehtava4

--- viikko6/Tennis/src/ohtu/TennisGame.java
+++ viikko6/Tennis/src/ohtu/TennisGame.java
@@ -1,80 +1,78 @@
 package ohtu;
 
 public class TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+
+    private int player1score = 0;
+    private int player2score = 0;
+    private final String player1;
+    private final String player2;
 
     public TennisGame(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.player1 = player1Name;
+        this.player2 = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals("player1")) {
+            player1score += 1;
+        } else {
+            player2score += 1;
+        }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        if (tie()) {
+            return buildEvenScore();
+        } else if (tieBreak()) {
+            return buildTieBreakScore();
+        } else {
+            return buildScore();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+    }
+
+    private String buildEvenScore() {
+        if (player1score < 4) {
+            return scoreToText(player1score) + "-All";
+        } else {
+            return "Deuce";
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+    }
+
+    private String buildTieBreakScore() {
+        int diff = player1score - player2score;
+        if (diff == 1) {
+            return "Advantage player1";
+        } else if (diff == -1) {
+            return "Advantage player2";
+        } else if (diff >= 2) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
         }
-        return score;
+    }
+
+    private String buildScore() {
+        return scoreToText(player1score) + "-" + scoreToText(player2score);
+    }
+
+    private String scoreToText(int score) {
+        switch (score) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            default:
+                return "Forty";
+        }
+    }
+
+    private boolean tie() {
+        return player1score == player2score;
+    }
+
+    private boolean tieBreak() {
+        return player1score >= 4 || player2score >= 4;
     }
 }
